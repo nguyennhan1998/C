af6fe84b85413b330b92344b386bce51e0a1fcef
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2019-08-02 17:06:22 +0700

    nhap vao hai so in ra so lon hon

---
+++ nhap vao hai so in ra so lon hon.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("\nnhap a: ");
+	scanf("%d",&a);
+	printf("\nnhap b: ");
+	scanf("%d",&b);
+	if(a>b)
+	{
+		printf("\nmax= %d",a);
+	}
+	else
+	printf("\nmax= %d",b);
+	return 0;
+}
