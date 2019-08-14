ef240b2d6c6f46c6bce79e3a6d3afb703366e643
nguyennhan1998 <53172541+nguyennhan1998@users.noreply.github.com>
2019-08-14 10:53:35 +0700

    bai 1

---
+++ exercise 1.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int sum(int a,int b)
+{
+	return a+b;
+}
+int sub(int a,int b)
+{
+	int hieu=a-b;
+	return hieu;
+}
+int multiplication(int a,int b)
+{
+
+	return a*b;
+}
+float division(int a,int b)
+{
+	return (float)a/b;
+}
+int main()
+{
+	int a,b;
+	printf("\nnhap a: ");
+	scanf("%d",&a);
+	printf("\nnhap b: ");
+	scanf("%d",&b);
+	printf("\ntong cua hai so la: %d",sum(a,b));
+	printf("\nhieu cua hai so la: %d",sub(a,b));
+	printf("\ntich cua hai so la: %d",multiplication(a,b));
+	printf("\nthuong cua hai so la: %f",division(a,b));
+	return 0;
+}
